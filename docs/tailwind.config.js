eada0b2f5db7737d1848e5056634acbadc40d997
Vasmaster <vasil.grancharov1996@gmail.com>
2025-03-15 12:45:15 +0200

    tailwindcss full build + old ref to v2.2.19

---
+++ docs/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html",
+    "./src/**/*.{html,js}",
+    "./docs/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
